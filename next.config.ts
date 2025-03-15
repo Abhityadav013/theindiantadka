bfc195b9abf2b0d226070dbd1dfa22cb8b67af20
Abhit Yadav <abhityadav013@gmail.com>
2025-03-16 01:35:57 +0530

    Update image domains in next.config.ts to include full URLs for improved asset loading

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,7 @@ import { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig:NextConfig = {
   images: {
-    domains: [
-      'testing.indiantadka.eu',
-      'google.com', // Remove 'https://'
-      'localhost', // Remove 'http://'
-    ]
+    domains: ['testing.indiantadka.eu','https://www.google.com/','http://localhost:5000/api/v1'], // Add your image domain here
   },
 };
 
